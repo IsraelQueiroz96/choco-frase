8a68039bb12a4dedc95a8eb2c50c351b85e4cdf6
Israel <isrelqueiroz668@gmail.com>
2022-04-30 16:52:01 -0300

    Adiciona

---
+++ mockup.js
@@ -0,0 +1,27 @@
+sortes = [
+    'Dá pra mim não',
+    'A gente vai ficar milionário',
+    'Marcuuuuuuuus',
+    'Dale bolo de rolo',
+    'Catei verdura que só o caraí',
+    'É o que porra',
+    'Muito tarado esse Flamengo',
+    'Rafinha deve ta suando virado no caraí',
+    'Respeita o príncipe do ninho do urubu',
+    'Mentira...mentira...mentira',
+    'Cala boca',
+    'Vão pra rua chupetas?',
+    'Vish maria',
+    'Tá gostando?',
+    'Bora, quero zuada, oxi',
+    'Dale cara de camelo',
+    'Calor do caraí',
+    'Rafinhaaaaaa',
+    'Resenha do caraí',
+    'O pirraia perdeu o busão?',
+    'Oxi pelo amor de Deus',
+    'Afim de garimpar véi'
+    
+];
+
+module.exports = sortes;
